e7622995ed70fa75e52222a6a6c223bdcc28d21f
Amr Monier <a.monier2107@gmail.com>
2023-01-09 01:09:05 +0200

    deployed services to local cluster

--- client/src/components/CreatePost.vue
+++ client/src/components/CreatePost.vue
@@ -1,44 +1,50 @@
 <template>
-    <div id="create_post" class="card p-3">
-        <form v-on:submit="submitForm">
-            <div class="mb-3">
-                <label for="title" class="form-label">Create Post</label>
-                <input id="title" type="text" class="form-control" v-model="title" placeholder="Post Title...">
-            </div>
-            <button type="submit" class="btn btn-outline-primary">Create </button>
-        </form>
-    </div>
+  <div id="create_post" class="card p-3">
+    <form v-on:submit="submitForm">
+      <div class="mb-3">
+        <label for="title" class="form-label">Create Post</label>
+        <input
+          id="title"
+          type="text"
+          class="form-control"
+          v-model="title"
+          placeholder="Post Title..."
+        />
+      </div>
+      <button type="submit" class="btn btn-outline-primary">Create</button>
+    </form>
+  </div>
 </template>
 <script lang="ts">
-import axios from 'axios'
-import { defineComponent } from 'vue'
+import axios from "axios";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    data() {
-        return {
-            title: ''
-        }
-    },
-    methods: {
-        async submitForm(e: any) {
-            console.log(this.title)
-            e.preventDefault()
-            try {
-                const { data } = await axios.post("http://localhost:4000/posts", {
-                    title: this.title
-                })
-                console.log(data)
-            } catch (error) {
-                window.alert("Something went wrong, couldn't create the post")
-            }
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    async submitForm(e: any) {
+      console.log(this.title);
+      e.preventDefault();
+      try {
+        const { data } = await axios.post("http://posts.com/posts/create", {
+          title: this.title,
+        });
+        console.log(data);
+      } catch (error) {
+        window.alert("Something went wrong, couldn't create the post");
+      }
 
-            this.title = ''
-        }
-    }
-})
+      this.title = "";
+    },
+  },
+});
 </script>
 <style scoped>
 .form-label {
-    color: #000;
+  color: #000;
 }
 </style>
--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -1,69 +1,90 @@
 <template>
-    <div class="container d-flex flex-column flex-wrap" id="posts">
-        <div class="card m-2" v-for="p in posts">
-            <div class="card-body">
-                <h5 class="card-title">{{ p.title }}</h5>
-                <p class="card-text">
-                <ul>
-                    <li v-for="c in p.comments"> {{ c.status === 'approved' ? c.content : c.status === 'pending' ?
-                            'comment waiting for moderation' : 'comment rejected'
-                    }}</li>
-                </ul>
-                </p>
-                <form v-on:submit.prevent="createComment(p)" :key="p.id">
-                    <div class="">
-                        <input type="text" class="form-control d-inline-block m-2 w-50" v-model="p.comment_content" placeholder="Comment...">
-                        <button type="submit" class=" btn btn-outline-success">Create Comment</button>
-                    </div>
-
-                </form>
-
+  <div class="container d-flex flex-column flex-wrap" id="posts">
+    <div class="card m-2" v-for="p in posts" :key="p.id">
+      <div class="card-body">
+        <h5 class="card-title">{{ p.title }}</h5>
+        <div class="card-text">
+          <ul>
+            <li v-for="c in p.comments" :key="c.id">
+              {{
+                c.status === "approved"
+                  ? c.content
+                  : c.status === "pending"
+                    ? "comment waiting for moderation"
+                    : "comment rejected"
+              }}
+            </li>
+          </ul>
+          <form v-on:submit.prevent="createComment(p)" :key="p.id">
+            <div class="">
+              <input type="text" class="form-control d-inline-block m-2 w-50" v-model="p.comment_content"
+                placeholder="Comment..." />
+              <button type="submit" class="btn btn-outline-success">
+                Create Comment
+              </button>
             </div>
+          </form>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 <script lang="ts">
+interface IPost {
+  id: string;
+  title: string;
+  comment_content: string;
+  comments: {
+    id: string;
+    content: string;
+    status: string;
+  }[];
+}
 import axios from "axios";
-import { defineComponent } from "vue"
+import { defineComponent } from "vue";
 export default defineComponent({
-    data() {
-        return {
-            posts: [{
-                id: "",
-                title: 'Post Title',
-                comment_content: '',
-                comments: [{
-                    id: '',
-                    content: '',
-                    status: ''
-                }]
-            }
-            ],
-            content: ''
-        }
+  name: "posts-list",
+  data() {
+    return {
+      posts: [] as IPost[],
+      content: "",
+    };
+  },
+  methods: {
+    async getPosts() {
+      const { data } = await axios.get("http://posts.com/posts");
+      this.posts = data;
     },
-    methods: {
-        async getPosts() {
-            const { data } = await axios.get("http://localhost:4000/posts")
-            this.posts = data
-        },
-        async createComment(post: any) {
-            const { data } = await axios.post("http://localhost:4000/comments", { post_id: post.id, content: post.comment_content })
-            console.log(data)
-            console.log(data[0].status === 'approved' ? data[0].content : data[0].status === 'pending' ?
-                'comment waiting for moderation' : 'comment rejected')
-            post.comments = data
-            post.comment_content = ''
+    async createComment(post: any) {
+      console.log(post);
+
+      const { data } = await axios.post(
+        `http://posts.com/posts/${post.id}/comments`,
+        {
+          post_id: post.id,
+          content: post.comment_content,
         }
+      );
+      console.log(data);
+      console.log(
+        data[0].status === "approved"
+          ? data[0].content
+          : data[0].status === "pending"
+            ? "comment waiting for moderation"
+            : "comment rejected"
+      );
+      post.comments = data;
+      post.comment_content = "";
     },
-    async mounted() {
-        await this.getPosts()
-    }
-})
+  },
+  async mounted() {
+    await this.getPosts();
+  },
+});
 </script>
 
 <style scoped>
 .card {
-    color: black;
+  color: black;
 }
 </style>
